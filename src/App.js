63a4137e9e2c472af89ab4bd736f9a7e31717122
bonarhyme <bonarhyme@gmail.com>
2021-05-23 15:45:13 +0100

    Initialized the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [res, setRes] = useState("");
+  const getData = async () => {
+    await fetch("https://random-african-quotes.herokuapp.com/api")
+      .then((response) => response.json())
+      .then((data) => setRes(data.api));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Data from backend: {res}</h2>
+      <button onClick={() => getData()}>GET DATA</button>
     </div>
   );
 }
